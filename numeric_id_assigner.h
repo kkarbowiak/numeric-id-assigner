ed72cd38bd662ba6bb878ca575b528bca834a78a
kkarbowiak <kkarbowiak@localhost>
2017-09-11 21:44:18 +0200

    Add initial implementation

---
+++ numeric_id_assigner.h
@@ -0,0 +1,55 @@
+#ifndef NUMERIC_ID_ASSIGNER
+#define NUMERIC_ID_ASSIGNER
+
+#include <map>
+#include <limits>
+
+
+template<typename I, typename O>
+class numeric_id_assigner
+{
+    public:
+        numeric_id_assigner();
+
+        void add(I id);
+        O get(I id) const;
+
+    private:
+        std::map<I, O> assignments;
+        O current;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+template<typename I, typename O>
+inline numeric_id_assigner<I, O>::numeric_id_assigner()
+  : assignments()
+  , current(0)
+{
+}
+////////////////////////////////////////////////////////////////////////////////
+template<typename I, typename O>
+inline void numeric_id_assigner<I, O>::add(I id)
+{
+    if (assignments.count(id) == 0)
+    {
+        assignments[id] = current++;
+    }
+}
+////////////////////////////////////////////////////////////////////////////////
+template<typename I, typename O>
+inline O numeric_id_assigner<I, O>::get(I id) const
+{
+    auto const it = assignments.find(id);
+    if (it != assignments.end())
+    {
+        return it->second;
+    }
+    else
+    {
+        return std::numeric_limits<O>::max();
+    }
+}
+////////////////////////////////////////////////////////////////////////////////
+
+#endif /* NUMERIC_ID_ASSIGNER */
+
